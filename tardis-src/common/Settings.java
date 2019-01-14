c5b6402817345b10d76ed4babecfa5b0538eef1e
Pietro Braione <pietro.braione@gmail.com>
2019-01-14 15:16:18 +0100

    Small change

--- tardis-src/common/Settings.java
+++ tardis-src/common/Settings.java
@@ -10,10 +10,10 @@ public final class Settings {
 	public static final Path Z3_PATH               = Paths.get("/opt", "local", "bin", "z3");	
 	
 	//these are good for everyone
-	public static final Path JBSE_PATH          = TARDIS_WORKSPACE.resolve("jbse/build/classes/java/main");
-	public static final Path EVOSUITE_PATH      = TARDIS_WORKSPACE.resolve("lib/evosuite-shaded-1.0.3.jar");
-	public static final Path EVOSUITE_MOSA_PATH = TARDIS_WORKSPACE.resolve("lib/evosuite-shaded-1.0.6-SNAPSHOT.jar");
-	public static final Path SUSHI_LIB_PATH     = TARDIS_WORKSPACE.resolve("runtime/build/classes/java/main");
+	public static final Path JBSE_PATH          = TARDIS_WORKSPACE.resolve(Paths.get("jbse", "build", "classes", "java", "main"));
+	public static final Path EVOSUITE_PATH      = TARDIS_WORKSPACE.resolve(Paths.get("lib", "evosuite-shaded-1.0.3.jar"));
+	public static final Path EVOSUITE_MOSA_PATH = TARDIS_WORKSPACE.resolve(Paths.get("lib", "evosuite-shaded-1.0.6-SNAPSHOT.jar"));
+	public static final Path SUSHI_LIB_PATH     = TARDIS_WORKSPACE.resolve(Paths.get("runtime", "build", "classes", "java", "main"));
 	public static final Path BIN_PATH           = EXPERIMENTS_WORKSPACE.resolve("bin");
 	public static final Path OUT_PATH           = EXPERIMENTS_WORKSPACE.resolve("tardis-test");
 	public static final Path TMP_BASE_PATH      = EXPERIMENTS_WORKSPACE.resolve("tardis-out");
